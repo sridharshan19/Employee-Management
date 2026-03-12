ac5ad21a842e943ece9b20a08c585a4ac773a0d2
sabarim6369 <sabarim6369@gmail.com>
2026-03-12 21:09:01 +0530

    d

--- employee-backend/config/db.js
+++ employee-backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/employee-management");
+    await mongoose.connect("mongodb+srv://sabarim63690122:Sabari.m6369@sabarim63690122.1oeqz.mongodb.net/employee-management");
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("❌ MongoDB connection failed:", err.message);
